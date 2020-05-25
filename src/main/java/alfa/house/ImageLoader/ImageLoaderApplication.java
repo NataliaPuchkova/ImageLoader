7a165b1e08fdf63d4961d22ccffcb761b895510e
alfalavista <natalia.puchkova@gmail.com>
2020-05-25 18:48:32 -0500

    First install

---
+++ src/main/java/alfa/house/ImageLoader/ImageLoaderApplication.java
@@ -0,0 +1,13 @@
+package alfa.house.ImageLoader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alfa/house/ImageLoader/ImageLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package alfa.house.ImageLoader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
