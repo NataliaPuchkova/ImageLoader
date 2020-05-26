6188c1a74b68956036b7f5fb69dc1ab93dbe57ca
alfalavista <natalia.puchkova@gmail.com>
2020-05-25 19:52:32 -0500

    Added upload functionality

---
+++ src/main/java/alfa/house/ImageLoader/controller/ImageLoaderController.java
@@ -0,0 +1,59 @@
+package alfa.house.ImageLoader.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@Api(value = "Image Loading", description = "Operation with tasks")
+public class ImageLoaderController {
+
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "//temp//";
+
+    @GetMapping(value = "/hello")
+    public String getHello(){
+        return "Hello world!";
+    }
+
+
+    @PostMapping("/upload") // //new annotation since 4.3
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+}
